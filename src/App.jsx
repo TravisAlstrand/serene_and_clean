f5bcbdd9dc5db57fcb70ca4c09565a2bbad2a65f
Travis Alstrand <92395688+TravisAlstrand@users.noreply.github.com>
2025-03-30 09:03:43 -0700

    components created, ready to start

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import "./App.css";
 
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Testimonials from "./components/Testimonials";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Hero />
+      <About />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import { MenuOutlined } from "@ant-design/icons";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="lg:hidden">
+        <MenuOutlined />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
