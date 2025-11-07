16ee6084ddc080059a76b7579daae10f2267c368
Travis Alstrand <92395688+TravisAlstrand@users.noreply.github.com>
2025-11-07 15:27:45 -0800

    Temporarily removed Testimonials section

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Services from "./components/Services";
-import Testimonials from "./components/Testimonials";
+// import Testimonials from "./components/Testimonials";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
@@ -15,7 +15,7 @@ function App() {
       <Hero />
       <About />
       <Services />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <Contact />
       <Footer />
     </div>
